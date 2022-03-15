992a50f791b0313611d4dc0bca752772d2563fe0
Jason Laster <jason.laster.11@gmail.com>
2022-03-15 10:28:26 -0700

    RM prePublishOnly.js

--- packages/playwright/bin/prePublishOnly.js
+++
@@ -1,8 +0,0 @@
-if (!require("fs").existsSync("./index.js")) {
-  console.error(
-    "\x1b[31mError:\x1b[37m Wrong Directory. Publish from the dist/ directory instead\n"
-  );
-  process.exit(1);
-}
-
-process.exit(0);
