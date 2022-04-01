878eee82b74ab95df1225e3900264505cb33c859
Ryan Duffy <ryan@replay.io>
2022-04-01 10:40:49 -0700

    Improve cli help

--- packages/playwright/bin/replayio-playwright.ts
+++ packages/playwright/bin/replayio-playwright.ts
@@ -2,16 +2,41 @@
 
 import install from "../src/install";
 
-const [, , cmd, ...args] = process.argv;
+let [, , cmd, ...args] = process.argv;
 
-if (
-  cmd === "install" ||
-  (cmd === "first-run" && !process.env.PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD)
-) {
+if (cmd === "first-run" && !process.env.PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD) {
+  args = [];
+  cmd = "install";
+}
+
+function commandInstall() {
   console.log("Installing Replay browsers for playwright");
 
   let browser = args[0] || "all";
   install(browser).then(() => {
     console.log("Done");
   });
+}
+
+function help() {
+  console.log(`
+npx @replayio/playwright
+
+Provides utilities to support using Replay (https://replay.io) with Playwright
+
+Available commands:
+
+  - install [all | firefox | chromium]
+    Installs all or the specified Replay browser
+  `);
+}
+
+switch (cmd) {
+  case "install":
+    commandInstall();
+    break;
+  case "help":
+  default:
+    help();
+    break;
 }
