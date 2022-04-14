bea63c345a17b371aeb888b8f391ce41fedc696c
Ryan Duffy <ryan@replay.io>
2022-04-14 14:54:47 -0700

    Add support for setting metadata via a file per worker

--- packages/playwright/src/index.ts
+++ packages/playwright/src/index.ts
@@ -1,20 +1,33 @@
 import { getPlaywrightBrowserPath, BrowserName } from "@replayio/replay";
+import { getDirectory } from "@replayio/replay/src/utils";
+import path from "path";
 
 function getDeviceConfig(browserName: BrowserName) {
-  const executablePath = getPlaywrightBrowserPath(browserName);
+  const executablePath = getExecutablePath(browserName);
   if (!executablePath) {
     console.warn(`${browserName} is not supported on this platform`);
+  }
+
+  const env: Record<string, any> = {
+    ...process.env,
+    RECORD_ALL_CONTENT: 1,
+  };
+
+  if (process.env.TEST_WORKER_INDEX && !("RECORD_REPLAY_METADATA" in env)) {
+    env.RECORD_REPLAY_METADATA_FILE = getMetadataFilePath(+process.env.TEST_WORKER_INDEX);
   }
 
   return {
     launchOptions: {
       executablePath,
-      env: {
-        RECORD_ALL_CONTENT: 1,
-      },
+      env,
     },
     defaultBrowserType: browserName,
   };
+}
+
+export function getMetadataFilePath(workerIndex = 0) {
+  return path.join(getDirectory(), `PLAYWRIGHT_METADATA_${workerIndex}`);
 }
 
 export function getExecutablePath(browserName: BrowserName) {
