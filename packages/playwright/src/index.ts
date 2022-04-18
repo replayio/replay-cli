a3146c042457a38410003967703be32da9bee36b
Ryan Duffy <ryan@replay.io>
2022-04-18 12:34:29 -0700

    improve messaging when both env variables are set

--- packages/playwright/src/index.ts
+++ packages/playwright/src/index.ts
@@ -13,8 +13,13 @@ function getDeviceConfig(browserName: BrowserName) {
     RECORD_ALL_CONTENT: 1,
   };
 
-  if (process.env.TEST_WORKER_INDEX && !("RECORD_REPLAY_METADATA" in env)) {
-    env.RECORD_REPLAY_METADATA_FILE = getMetadataFilePath(+process.env.TEST_WORKER_INDEX);
+  if (process.env.TEST_WORKER_INDEX) {
+    if ("RECORD_REPLAY_METADATA" in env && env.RECORD_REPLAY_METADATA) {
+      console.warn(`RECORD_REPLAY_METADATA is set so a per-worker metadata file will not be used`);
+    } else {
+      env.RECORD_REPLAY_METADATA = undefined;
+      env.RECORD_REPLAY_METADATA_FILE = getMetadataFilePath(+process.env.TEST_WORKER_INDEX);
+    }
   }
 
   return {
