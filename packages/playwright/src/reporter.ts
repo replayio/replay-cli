467bddad627e455fdca8a71eeacb2e103dc87124
Ryan Duffy <ryan@replay.io>
2022-04-20 16:22:59 -0700

    Fix updating metadata when multiple recordings have the same testId (#18)

--- packages/playwright/src/reporter.ts
+++ packages/playwright/src/reporter.ts
@@ -1,4 +1,10 @@
-import type { FullConfig, Reporter, Suite, TestCase, TestResult } from "@playwright/test/reporter";
+import type {
+  FullConfig,
+  Reporter,
+  Suite,
+  TestCase,
+  TestResult,
+} from "@playwright/test/reporter";
 import { getDirectory } from "@replayio/replay/src/utils";
 import { listAllRecordings } from "@replayio/replay";
 import { writeFileSync, appendFileSync, existsSync } from "fs";
@@ -7,7 +13,7 @@ import path from "path";
 import { getMetadataFilePath } from "./index";
 
 class ReplayReporter implements Reporter {
-  baseId = Date.now()
+  baseId = Date.now();
 
   getTestId(test: TestCase) {
     return `${this.baseId}-${test.titlePath().join("-")}`;
@@ -23,31 +29,43 @@ class ReplayReporter implements Reporter {
   onTestBegin(test: TestCase, testResult: TestResult) {
     const metadataFilePath = getMetadataFilePath(testResult.workerIndex);
     if (existsSync(metadataFilePath)) {
-      writeFileSync(metadataFilePath, JSON.stringify({
-        testId: this.getTestId(test)
-      }, undefined, 2), {});
+      writeFileSync(
+        metadataFilePath,
+        JSON.stringify(
+          {
+            testId: this.getTestId(test),
+          },
+          undefined,
+          2
+        ),
+        {}
+      );
     }
   }
 
   onTestEnd(test: TestCase, result: TestResult) {
     if (!["passed", "failed"].includes(result.status)) return;
 
-    const rec = listAllRecordings().find(r => r.metadata.testId === this.getTestId(test))
-    if (rec) {
-      const metadata = {
-        id: rec.id,
-        kind: "addMetadata",
-        metadata: {
-          title: test.title,
-          testStatus: result.status,
-        },
-        timestamp: Date.now(),
-      };
+    const recs = listAllRecordings().filter(
+      (r) => r.metadata.testId === this.getTestId(test)
+    );
+    if (recs.length > 0) {
+      recs.forEach((rec) => {
+        const metadata = {
+          id: rec.id,
+          kind: "addMetadata",
+          metadata: {
+            title: test.title,
+            testStatus: result.status,
+          },
+          timestamp: Date.now(),
+        };
 
-      appendFileSync(
-        path.join(getDirectory(), "recordings.log"),
-        `\n${JSON.stringify(metadata)}\n`
-      );
+        appendFileSync(
+          path.join(getDirectory(), "recordings.log"),
+          `\n${JSON.stringify(metadata)}\n`
+        );
+      });
     }
   }
 }
