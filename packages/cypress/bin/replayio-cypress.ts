305eb3762a62b4530c824359c3ce03755a93d327
Ryan Duffy <ryan@replay.io>
2022-06-21 10:18:22 -0700

    Fix install script for cypress

---
+++ packages/cypress/bin/replayio-cypress.ts
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+import install from "../src/install";
+
+let [, , cmd, ...args] = process.argv;
+
+if (cmd === "first-run" && process.env.CYPRESS_INSTALL_BINARY !== "0") {
+  args = [];
+  cmd = "install";
+}
+
+function commandInstall() {
+  console.log("Installing Replay browsers for cypress");
+
+  let browser = args[0] || "all";
+  install(browser).then(() => {
+    console.log("Done");
+  });
+}
+
+function help() {
+  console.log(`
+npx @replayio/cypress
+
+Provides utilities to support using Replay (https://replay.io) with Cypress
+
+Available commands:
+
+  - install [all | firefox | chromium]
+    Installs all or the specified Replay browser
+  `);
+}
+
+switch (cmd) {
+  case "install":
+    commandInstall();
+    break;
+  case "help":
+  default:
+    help();
+    break;
+}
