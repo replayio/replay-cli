e5c8652a86d5f0a518d0c6718949ab01da01cfa1
Dan Miller <dan@replay.io>
2022-07-15 22:26:52 -0700

    once again I don't know why this doesn't work in the container but it doesn't

--- packages/cypress/src/index.test.ts
+++ packages/cypress/src/index.test.ts
@@ -1,13 +1,15 @@
 jest.mock("@replayio/replay/");
-const { getPlaywrightBrowserPath } = require("@replayio/replay");
+import { getPlaywrightBrowserPath } from "@replayio/replay";
 
+// we need to import these manually because jest doesn't play nicely with cypress
 import { expect, jest } from "@jest/globals";
 
 import plugin from "./index";
 
 describe("plugin", () => {
   it("throws if it can't find chromium or firefox", () => {
-    getPlaywrightBrowserPath.mockImplementation(() => undefined);
+    const fn: any = getPlaywrightBrowserPath;
+    fn.mockImplementation(() => undefined);
     const on = jest.fn();
     const config: any = {
       browsers: [],
