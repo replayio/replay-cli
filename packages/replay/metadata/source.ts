94fe96a616bceaecf0566e2ff5e9ea5af5043c36
Ryan Duffy <ryan@replay.io>
2022-07-19 17:39:35 -0700

    Attempt to populate source metadata from know environment variables (#66)
    * Add environment variable pre-population to source metadata
    
    * tweak env variable defaults
    
    * make merge id optional
    
    * add unit tests
    
    * simplify tests

--- packages/replay/metadata/source.ts
+++ packages/replay/metadata/source.ts
@@ -1,35 +1,94 @@
 import type { Struct } from "superstruct";
-const { create, object, optional, string } = require("superstruct");
+const { create, object, optional, defaulted } = require("superstruct");
 
+import { envString } from "./env";
 import { UnstructuredMetadata } from "./types";
+
+const defaultObject = (objStruct: any) => optional(defaulted(object(objStruct), {}));
 
 const VERSION = 1;
 
 const versions: Record<number, Struct> = {
   1: object({
-    branch: optional(string()),
-    commit: object({
-      id: string(),
-      title: optional(string()),
-      url: optional(string()),
+    branch: optional(
+      envString(
+        "RECORD_REPLAY_METADATA_SOURCE_BRANCH",
+        "GITHUB_REF_NAME",
+        "BUILDKITE_BRANCH",
+        "CIRCLE_BRANCH"
+      )
+    ),
+    commit: defaultObject({
+      id: envString(
+        "RECORD_REPLAY_METADATA_SOURCE_COMMIT_ID",
+        "GITHUB_SHA",
+        "BUILDKITE_COMMIT",
+        "CIRCLE_SHA1"
+      ),
+      title: optional(envString("RECORD_REPLAY_METADATA_SOURCE_COMMIT_TITLE")),
+      url: optional(envString("RECORD_REPLAY_METADATA_SOURCE_COMMIT_URL")),
     }),
-    trigger: optional(
-      object({
-        user: optional(string()),
-        name: string(),
-        workflow: optional(string()),
-        url: optional(string()),
-      })
+    trigger: defaultObject({
+      user: optional(
+        envString(
+          "RECORD_REPLAY_METADATA_SOURCE_TRIGGER_USER",
+          "GITHUB_ACTOR",
+          "BUILDKITE_BUILD_CREATOR",
+          "BUILDKITE_BUILD_AUTHOR",
+          "CIRCLE_USERNAME",
+          "CIRCLE_PR_USERNAME"
+        )
+      ),
+      name: optional(envString("RECORD_REPLAY_METADATA_SOURCE_TRIGGER_NAME")),
+      workflow: optional(
+        envString(
+          "RECORD_REPLAY_METADATA_SOURCE_TRIGGER_WORKFLOW",
+          "GITHUB_RUN_ID",
+          "BUILDKITE_BUILD_NUMBER",
+          "CIRCLE_BUILD_NUM"
+        )
+      ),
+      url: optional(
+        envString(
+          "RECORD_REPLAY_METADATA_SOURCE_TRIGGER_URL",
+          env =>
+            env.GITHUB_WORKFLOW &&
+            `${env.GITHUB_SERVER_URL}/${env.GITHUB_REPOSITORY}/actions/runs/${env.GITHUB_RUN_ID}`,
+          "BUILDKITE_BUILD_URL",
+          "CIRCLE_BUILD_URL"
+        )
+      ),
+    }),
+    merge: defaultObject({
+      id: optional(
+        envString("RECORD_REPLAY_METADATA_SOURCE_MERGE_ID", "BUILDKITE_PULL_REQUEST", env =>
+          process.env.CIRCLE_PULL_REQUEST?.split("/").pop()
+        )
+      ),
+      title: optional(envString("RECORD_REPLAY_METADATA_SOURCE_MERGE_TITLE")),
+      url: optional(envString("RECORD_REPLAY_METADATA_SOURCE_MERGE_URL")),
+    }),
+    provider: optional(
+      envString(
+        "RECORD_REPLAY_METADATA_SOURCE_PROVIDER",
+        env => env.GITHUB_WORKFLOW && "github",
+        "BUILDKITE_PIPELINE_PROVIDER",
+        env =>
+          env.CIRCLE_REPOSITORY_URL?.includes("github.com")
+            ? "github"
+            : env.CIRCLE_REPOSITORY_URL?.includes("bitbucket.com")
+            ? "bitbucket"
+            : undefined
+      )
     ),
-    merge: optional(
-      object({
-        id: string(),
-        title: string(),
-        url: optional(string()),
-      })
+    repository: optional(
+      envString(
+        "RECORD_REPLAY_METADATA_SOURCE_REPOSITORY",
+        "GITHUB_REPOSITORY",
+        env => env.BUILDKITE_REPO?.match(/.*:(.*)\.git/)?.[1],
+        "CIRCLE_PROJECT_REPONAME"
+      )
     ),
-    provider: optional(string()),
-    repository: optional(string()),
   }),
 };
 
@@ -41,7 +100,7 @@ function validate(metadata: { source: UnstructuredMetadata }) {
   return init(metadata.source);
 }
 
-function init(data: UnstructuredMetadata) {
+function init(data: UnstructuredMetadata = {}) {
   const version = typeof data.version === "number" ? data.version : VERSION;
   if (versions[version]) {
     return {
