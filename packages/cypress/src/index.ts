ac3f9aee7cc6275cadf764fbfb2d380d1f1b2d3a
Ryan Duffy <ryan@replay.io>
2022-09-09 11:10:19 -0700

    add version check

--- packages/cypress/src/index.ts
+++ packages/cypress/src/index.ts
@@ -12,17 +12,20 @@ const plugin: Cypress.PluginConfig = (on, config) => {
     selectedBrowser = browser.family;
     reporter.onTestSuiteBegin(undefined, "CYPRESS_REPLAY_METADATA");
 
-    return {
-      ...launchOptions,
-      env: {
-        RECORD_REPLAY_DRIVER:
-          process.env.RECORD_REPLAY_NO_RECORD && selectedBrowser === "chromium"
-            ? __filename
-            : undefined,
-        RECORD_ALL_CONTENT: process.env.RECORD_REPLAY_NO_RECORD ? undefined : 1,
-        RECORD_REPLAY_METADATA_FILE: getMetadataFilePath(),
-      },
-    };
+    const [major, minor] = config.version.split(".");
+    if (major && Number.parseInt(major) >= 10 && minor && Number.parseInt(minor) >= 8) {
+      return {
+        ...launchOptions,
+        env: {
+          RECORD_REPLAY_DRIVER:
+            process.env.RECORD_REPLAY_NO_RECORD && selectedBrowser === "chromium"
+              ? __filename
+              : undefined,
+          RECORD_ALL_CONTENT: process.env.RECORD_REPLAY_NO_RECORD ? undefined : 1,
+          RECORD_REPLAY_METADATA_FILE: getMetadataFilePath(),
+        },
+      };
+    }
   });
   on("before:spec", () => reporter.onTestBegin(undefined, getMetadataFilePath()));
   on("after:spec", (spec, result) => {
