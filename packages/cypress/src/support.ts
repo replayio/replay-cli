44b7a69f03420de67abb308f52f934942a85b7f0
Ryan Duffy <ryan@replay.io>
2022-11-21 15:59:05 -0800

    rework log handling

--- packages/cypress/src/support.ts
+++ packages/cypress/src/support.ts
@@ -61,6 +61,7 @@ const handleCypressEvent = (event: StepEvent["event"], cmd?: CommandLike, error?
 
 const idMap: Record<string, string> = {};
 let gReplayIndex = 1;
+
 const getReplayId = (cypressId: string) => {
   return (idMap[cypressId] = idMap[cypressId] || String(gReplayIndex++));
 };
@@ -138,38 +139,55 @@ export default function register() {
   });
   Cypress.on("log:added", log => {
     // We only care about asserts
-    if (log.name === "assert") {
-      const cmd = {
-        name: log.name,
-        id: getReplayId(nextAssertion?.id ?? log.id),
-        groupId: log.chainerId && getReplayId(log.chainerId),
-        args: [log.consoleProps.Message],
-      };
-      addAnnotation("step:start", { commandVariable: "cmd", id: cmd.id });
-      handleCypressEvent("step:start", cmd);
+    if (log.name !== "assert") {
+      return;
     }
-  });
-  Cypress.on("log:changed", log => {
-    // We only care about asserts
-    if (log.name === "assert" && ["passed", "failed"].includes(log.state)) {
-      const cmd = {
-        name: log.name,
-        id: getReplayId(log.id),
-        args: [log.consoleProps.Message],
+
+    const replayId = getReplayId(nextAssertion?.get("id") || log.id);
+
+    const cmd = {
+      name: log.name,
+      id: replayId,
+      groupId: log.chainerId && getReplayId(log.chainerId),
+      args: [log.consoleProps.Message],
+    };
+    addAnnotation("step:start", {
+      commandVariable: nextAssertion ? "nextAssertion" : undefined,
+      logVariable: "log",
+      id: cmd.id,
+    });
+    handleCypressEvent("step:start", cmd);
+
+    const logChanged = (changedLog: any) => {
+      if (changedLog.id !== log.id || !["passed", "failed"].includes(changedLog.state)) return;
+
+      // We only care about asserts
+      const changedCmd = {
+        ...cmd,
+        // Update args which can be updated when an assert resolves
+        args: [changedLog.consoleProps.Message],
       };
 
-      const error = log.err
+      const error = changedLog.err
         ? {
-            name: log.err.name,
-            message: log.err.message,
-            line: log.err.codeFrame?.line,
-            column: log.err.codeFrame?.column,
+            name: changedLog.err.name,
+            message: changedLog.err.message,
+            line: changedLog.err.codeFrame?.line,
+            column: changedLog.err.codeFrame?.column,
           }
         : undefined;
 
-      addAnnotation("step:end", { commandVariable: "cmd", id: cmd.id });
-      handleCypressEvent("step:end", cmd, error);
-    }
+      addAnnotation("step:end", {
+        commandVariable: nextAssertion ? "nextAssertion" : undefined,
+        logVariable: "changedLog",
+        id: changedCmd.id,
+      });
+      handleCypressEvent("step:end", changedCmd, error);
+
+      Cypress.off("logchanged", logChanged);
+    };
+
+    Cypress.on("log:changed", logChanged);
   });
   beforeEach(() => {
     handleCypressEvent("test:start");
