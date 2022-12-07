6af937fb23f1043be02e6a299f6cc323db349de3
Ryan Duffy <ryan@replay.io>
2022-12-07 10:31:50 -0800

    Add test harness for cypress events

---
+++ packages/cypress/src/fixture.ts
@@ -0,0 +1,28 @@
+import { writeFileSync, appendFileSync } from "fs";
+import path from "path";
+
+function getFixtureFile() {
+  return path.resolve(__filename, "../../../tests/fixtures/fixture.log");
+}
+
+export function initFixtureFile() {
+  if (process.env.REPLAY_UPDATE_FIXTURE) {
+    try {
+      writeFileSync(getFixtureFile(), "");
+    } catch (e) {
+      console.error(e);
+      process.env.REPLAY_UPDATE_FIXTURE = undefined;
+    }
+  }
+}
+
+export function appendToFixtureFile(type: string, value: any) {
+  if (process.env.REPLAY_UPDATE_FIXTURE) {
+    try {
+      appendFileSync(getFixtureFile(), JSON.stringify({ type, value }) + "\n");
+    } catch (e) {
+      console.error(e);
+      process.env.REPLAY_UPDATE_FIXTURE = undefined;
+    }
+  }
+}
