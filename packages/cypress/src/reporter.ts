e25fc9d5e945fba1309eff3b625b1c9de1561557
Ryan Duffy <ryan@replay.io>
2023-02-06 10:20:02 -0800

    Add debug output for cypress and test-utils (#144)

--- packages/cypress/src/reporter.ts
+++ packages/cypress/src/reporter.ts
@@ -1,13 +1,19 @@
 /// <reference types="cypress" />
 
 import type { Test } from "@replayio/test-utils";
+import debug from "debug";
 import type { StepEvent } from "./support";
 import { groupStepsByTest } from "./steps";
 
 class CypressReporter {
+  debug: debug.Debugger;
   startTime: number | undefined;
   steps: StepEvent[] = [];
   selectedBrowser: "chromium" | "firefox" | undefined;
+
+  constructor(debug: debug.Debugger) {
+    this.debug = debug.extend("reporter");
+  }
 
   setStartTime(startTime: number) {
     this.startTime = startTime;
@@ -33,19 +39,31 @@ class CypressReporter {
       result.tests.length === 0 ||
       result.tests.every(t => t.state === "pending")
     ) {
+      this.debug("No test results found for spec %s", spec.relative);
       return [];
     }
 
     let testsWithSteps: Test[] = [];
     try {
-      testsWithSteps = groupStepsByTest(this.steps, this.startTime!);
+      testsWithSteps = groupStepsByTest(this.steps, this.startTime!, this.debug);
     } catch (e) {
       console.warn("Failed to build test step metadata for this replay.");
       console.warn(e);
     }
 
     const tests = result.tests.map<Test>(t => {
-      const foundTest = testsWithSteps.find(ts => ts.title === t.title[t.title.length - 1]) || null;
+      const foundTest = testsWithSteps.find(ts => ts.title === t.title[t.title.length - 1]);
+
+      if (foundTest) {
+        this.debug("Matching test result with test steps from support: %o", {
+          testResult: t.title,
+          testWithSteps: foundTest.path,
+        });
+      } else {
+        this.debug("Failed to find matching test steps for test result: %o", {
+          testResult: t.title,
+        });
+      }
 
       const error = t.displayError
         ? {
