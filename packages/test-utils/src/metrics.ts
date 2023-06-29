42b1c23fc1d975ca5f9d3aba106fa40ab587b7ef
Ryan Duffy <ryan@replay.io>
2023-06-29 13:27:43 -0700

    Fix sending test metrics after v2 metadata change

--- packages/test-utils/src/metrics.ts
+++ packages/test-utils/src/metrics.ts
@@ -1,5 +1,10 @@
+import dbg from "debug";
 import os from "os";
 import fetch from "node-fetch";
+import { Test } from "./reporter";
+import { TestMetadataV2 } from "@replayio/replay/metadata/test/v2";
+
+const debug = dbg("replay:test-utils:metrics");
 
 function shouldReportTestMetrics() {
   const optOut = process.env.RECORD_REPLAY_TEST_METRICS?.toLowerCase();
@@ -12,12 +17,9 @@ async function pingTestMetrics(
   runId: string,
   test: {
     id: string;
-    duration: number;
+    approximateDuration: number;
     recorded: boolean;
-    source: {
-      title: string;
-      scope: string[];
-    };
+    source: TestMetadataV2.TestRun["source"];
     runtime?: string;
     runner?: string;
     result?: string;
@@ -27,24 +29,28 @@ async function pingTestMetrics(
 
   const webhookUrl = process.env.RECORD_REPLAY_WEBHOOK_URL || "https://webhooks.replay.io";
 
+  const body = JSON.stringify({
+    type: "test.finished",
+    recordingId,
+    test: {
+      ...test,
+      platform: os.platform,
+      runId,
+      env: {
+        disableAsserts: !!process.env.RECORD_REPLAY_DISABLE_ASSERTS,
+        disableSourcemapCollection: !!process.env.RECORD_REPLAY_DISABLE_SOURCEMAP_COLLECTION,
+        disableFeatures: process.env.RECORD_REPLAY_DISABLE_FEATURES || "none",
+      },
+    },
+  });
+
+  debug(body);
+
   try {
     return await fetch(`${webhookUrl}/api/metrics`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        type: "test.finished",
-        recordingId,
-        test: {
-          ...test,
-          platform: os.platform,
-          runId,
-          env: {
-            disableAsserts: !!process.env.RECORD_REPLAY_DISABLE_ASSERTS,
-            disableSourcemapCollection: !!process.env.RECORD_REPLAY_DISABLE_SOURCEMAP_COLLECTION,
-            disableFeatures: process.env.RECORD_REPLAY_DISABLE_FEATURES || "none",
-          },
-        },
-      }),
+      body,
     });
   } catch (e) {
     console.log("Failed to send test metrics", e);
