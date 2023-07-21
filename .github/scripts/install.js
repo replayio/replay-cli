ee08278c564db6649dc8acdd54932d8586973267
Ryan Duffy <ryan@replay.io>
2023-07-21 12:29:32 -0700

    log cypress plugin

--- .github/scripts/install.js
+++ .github/scripts/install.js
@@ -5,14 +5,25 @@ const baseDir = require("../../packages/replay/dist/src/utils").getDirectory();
 const dir = path.join(baseDir, "runtimes");
 
 if (fs.existsSync(dir)) {
-  console.log(dir);
+  console.log(`\nInstalled runtimes in ${dir}`);
   console.log(
     fs
       .readdirSync(dir)
+      .filter(f => f !== "profiles")
       .map(d => `  ${d}`)
       .join("\n")
   );
 } else {
   console.error(dir, "does not exist");
+  exit(1);
+}
+
+const plugin = require("../../packages/cypress/dist").default;
+const config = plugin(() => {}, { env: {} });
+
+if (config && !(config instanceof Promise)) {
+  console.log("\nCypress browsers");
+  console.log(config.browsers?.map(b => `  ${b.name}: ${b.path}`).join("\n"));
+} else {
   exit(1);
 }
