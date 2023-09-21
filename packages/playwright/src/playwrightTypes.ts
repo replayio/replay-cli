24f533c30ca9d0a7aa21d9c71d688a78dc2389ef
Ryan Duffy <ryan@replay.io>
2023-09-21 16:07:44 -0700

    Add fixture for playwright annotations (#238)
    * Add stacks to test steps
    
    * Pass on rrId
    
    * Add fixture for playwright annotations
    
    * simplify
    
    * wip: add websocket to communicate between fixture and reporter
    
    * plumb fixture data to reporter
    
    * update schema version
    
    * Put stacks in x-replay-playwright
    
    * undo package-lock changes
    
    * rename
    
    * fix port def
    
    * move messages into separate debug
    
    * export fixture members
    
    * add data to annotations
    
    * reorder imports
    
    * refactoring fixture handling into separate methods
    
    * add step:end
    
    * add fixture errors
    
    * fix args
    
    * add monkey patch for expects
    
    * consolidate logic and add error handling
    
    * Add support for parallel runs
    
    * fix promise rejection
    
    ---------
    
    Co-authored-by: Holger Benl <hbenl@evandor.de>

---
+++ packages/playwright/src/playwrightTypes.ts
@@ -0,0 +1,55 @@
+// Types imported from playwright and used by our fixture
+
+import { APIRequestContext, BrowserContext, TestInfo } from "@playwright/test";
+
+export interface TestInfoInternal extends TestInfo {
+  _addStep?: (data: any, parentStep: any) => TestInfoStep;
+  _onStepEnd?: (step: {
+    testId: string;
+    stepId: string;
+    wallTime: number;
+    error?: {
+      message: string;
+      stack: string;
+    };
+  }) => void;
+}
+
+export interface TestInfoStep {
+  stepId: string;
+  location: StackFrame;
+  category: string;
+  title: string;
+  params?: Record<string, any>;
+  wallTime: number;
+}
+
+export type StackFrame = {
+  file: string;
+  line: number;
+  column: number;
+  function?: string;
+};
+
+export type ParsedStackTrace = {
+  allFrames: StackFrame[];
+  frames: StackFrame[];
+  frameTexts: string[];
+  apiName: string | undefined;
+};
+
+export interface ClientInstrumentationListener {
+  onApiCallBegin?(
+    apiCall: string,
+    params: Record<string, any>,
+    stackTrace: ParsedStackTrace | null,
+    wallTime: number,
+    userData: any
+  ): void;
+  onApiCallEnd?(userData: any, error?: Error): void;
+  onDidCreateBrowserContext?(context: BrowserContext): Promise<void>;
+  onDidCreateRequestContext?(context: APIRequestContext): Promise<void>;
+  onWillPause?(): void;
+  onWillCloseBrowserContext?(context: BrowserContext): Promise<void>;
+  onWillCloseRequestContext?(context: APIRequestContext): Promise<void>;
+}
