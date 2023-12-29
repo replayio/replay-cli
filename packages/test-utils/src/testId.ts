98c4e57fba72dbf9dba1b2c14fee1d8146c2dc7b
Ryan Duffy <ryan@replay.io>
2023-12-29 15:54:07 -0800

    add node fallback

--- packages/test-utils/src/testId.ts
+++ packages/test-utils/src/testId.ts
@@ -8,9 +8,13 @@ export function buildTestId(
 }
 
 export async function generateOpaqueId(contents: string): Promise<string> {
-  const enc = new TextEncoder();
-  const hash = await crypto.subtle.digest("SHA-1", enc.encode(contents));
-  return Array.from(new Uint8Array(hash))
-    .map(v => v.toString(16).padStart(2, "0"))
-    .join("");
+  if (globalThis.window) {
+    const enc = new TextEncoder();
+    const hash = await crypto.subtle.digest("SHA-1", enc.encode(contents));
+    return Array.from(new Uint8Array(hash))
+      .map(v => v.toString(16).padStart(2, "0"))
+      .join("");
+  } else {
+    return require("crypto").createHash("sha1").update(contents).digest("hex");
+  }
 }
