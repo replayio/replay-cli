b9583c557b875195a37ec649059d7912f1e8e251
Ryan Duffy <ryan@replay.io>
2023-12-29 16:38:50 -0800

    cleanup

--- packages/test-utils/src/testId.ts
+++ packages/test-utils/src/testId.ts
@@ -8,13 +8,15 @@ export function buildTestId(
 }
 
 export async function generateOpaqueId(contents: string): Promise<string> {
-  if (globalThis.window) {
+  if (globalThis.crypto) {
+    // In the browser, use the global crypto obj to generate the sha-1 hash
     const enc = new TextEncoder();
     const hash = await crypto.subtle.digest("SHA-1", enc.encode(contents));
     return Array.from(new Uint8Array(hash))
       .map(v => v.toString(16).padStart(2, "0"))
       .join("");
   } else {
+    // In node, rely on the crypto package instead
     return require("crypto").createHash("sha1").update(contents).digest("hex");
   }
 }
