45c2b9cc2cfa83a37588f8497c49f0e48e16e689
Ryan Duffy <ryan@replay.io>
2024-01-19 11:32:32 -0800

    add CLI output message for log file

--- packages/replay/src/debug.ts
+++ packages/replay/src/debug.ts
@@ -7,7 +7,7 @@ import { getDirectory } from "./utils";
 const debugDebug = dbg("replay:cli:debug");
 
 const logDirPath = path.join(getDirectory(), "logs");
-const logPath = path.join(
+let logPath = path.join(
   logDirPath,
   "cli-" +
     new Date()
@@ -20,6 +20,7 @@ function init() {
   try {
     fs.mkdirSync(logDirPath, { recursive: true });
   } catch (e) {
+    logPath = "";
     debugDebug("Failed to create log directory %o", e);
   }
 }
@@ -35,18 +36,27 @@ export default function debug(namespace: string) {
 
   return (formatter: string, ...args: any[]) => {
     d(formatter, ...args);
-    try {
-      const output = util
-        .format(formatter, ...args)
-        .split("\n")
-        .map((l, i) => (i === 0 ? l : "".padStart(size + 3, " ") + l))
-        .join("\n");
-      const prefix = `[${namespace}] `.padStart(size + 3, " ");
-      fs.appendFileSync(logPath, `${prefix}${output}\n`);
-    } catch (e) {
-      debugDebug("Failed to write log %o", e);
+
+    if (logPath) {
+      try {
+        const output = util
+          .format(formatter, ...args)
+          .split("\n")
+          .map((l, i) => (i === 0 ? l : "".padStart(size + 3, " ") + l))
+          .join("\n");
+        const prefix = `[${namespace}] `.padStart(size + 3, " ");
+        fs.appendFileSync(logPath, `${prefix}${output}\n`);
+      } catch (e) {
+        debugDebug("Failed to write log %o", e);
+      }
     }
   };
+}
+
+export function printLogPath() {
+  if (logPath && fs.existsSync(logPath)) {
+    console.error("Additional logs available in", logPath);
+  }
 }
 
 init();
