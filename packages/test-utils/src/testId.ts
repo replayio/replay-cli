07e6d3db2629c1fb771fe6b77cfc5fecb2c9a244
Ryan Duffy <ryan@replay.io>
2024-02-09 17:25:57 -0800

    add id cache

--- packages/test-utils/src/testId.ts
+++ packages/test-utils/src/testId.ts
@@ -9,13 +9,23 @@ export function buildTestId(sourcePath: string, test: Pick<Test, "id" | "source"
   return id;
 }
 
+const gIdCache = new Map<string, string>();
+
 export function generateOpaqueId(contents: string): string {
+  if (gIdCache.has(contents)) {
+    return gIdCache.get(contents)!;
+  }
+
+  let id: string;
   if (globalThis.window) {
     // in the browser, we're using this sync sha-1 lib because the built-in
     // crypto is async which causes problems with Cypress
-    return sha1(contents);
+    id = sha1(contents);
   } else {
     // In node, rely on the crypto package instead
-    return require("crypto").createHash("sha1").update(contents).digest("hex");
+    id = require("crypto").createHash("sha1").update(contents).digest("hex");
   }
+
+  gIdCache.set(contents, id);
+  return id;
 }
