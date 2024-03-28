22e473ff1e7d41462621286fffcaece7fcaf2b43
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-03-28 17:11:57 +0100

    Add extra entrypoints to allow packages to avoid pulling to many dependencies (#342)

--- packages/cypress/src/support.ts
+++ packages/cypress/src/support.ts
@@ -1,5 +1,5 @@
 import type { TestMetadataV2 } from "@replayio/test-utils";
-import { buildTestId } from "@replayio/test-utils";
+import { buildTestId } from "@replayio/test-utils/testId";
 import { CONNECT_TASK_NAME } from "./constants";
 import { PluginFeature, isFeatureEnabled } from "./features";
 
