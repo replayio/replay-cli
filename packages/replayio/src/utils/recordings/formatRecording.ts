769a4d89d2238abcb869fa3a804053a5df8a5fef
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-04-08 14:26:11 -0400

    Better format NodeJS recordings in 'list'

--- packages/replayio/src/utils/recordings/formatRecording.ts
+++ packages/replayio/src/utils/recordings/formatRecording.ts
@@ -1,4 +1,5 @@
 import { formatDuration, formatRelativeDate } from "../date";
+import { parseBuildId } from "../installation/parseBuildId";
 import { dim, link } from "../theme";
 import { LocalRecording } from "./types";
 
@@ -7,22 +8,33 @@ const MAX_TITLE_LENGTH = 35;
 export function formatRecording(recording: LocalRecording) {
   const id = recording.id.substring(0, 8) + "…";
 
+  const { runtime } = parseBuildId(recording.buildId);
+
   let title;
-  if (recording.metadata.host) {
-    if (recording.metadata.host.length > MAX_TITLE_LENGTH) {
-      title = link(recording.metadata.host.substring(0, MAX_TITLE_LENGTH).trimEnd() + "…");
-    } else {
-      title = link(recording.metadata.host);
+  switch (runtime) {
+    case "node": {
+      title = "NodeJS";
+      break;
     }
-  } else {
-    title = "(untitled)";
+    default: {
+      if (recording.metadata.host) {
+        if (recording.metadata.host.length > MAX_TITLE_LENGTH) {
+          title = link(recording.metadata.host.substring(0, MAX_TITLE_LENGTH).trimEnd() + "…");
+        } else {
+          title = link(recording.metadata.host);
+        }
+      } else {
+        title = "(untitled)";
+      }
+      break;
+    }
   }
 
   const date = dim(formatRelativeDate(recording.date));
   const duration = dim(recording.duration ? formatDuration(recording.duration) : "");
-  const processType = recording.metadata.processType
-    ? dim(`(${recording.metadata.processType})`)
-    : "";
+  const processType = dim(
+    recording.metadata.processType ? `(${recording.metadata.processType})` : ""
+  );
 
   let status;
 
