bae05d161b5854af33d270f9ca726be5f5e83cb7
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-04-09 13:58:19 -0400

    Refine update prompt logic (#359)
    The new CLI will prompt the user to update either the CLI itself or the runtime if either is out of date. I added logic initially so that we wouldn't ask more than once a day (because that would be annoying) but I realized that we _should_ probably ask again if _a new_ release came out sooner than a day. (For example, if we do a roll back of the Runtime or something?)
    
    So this commit changes the "should we prompt again?" logic to check both _when_ we last asked and what the last value we suggested updating to was.
    
    ---
    
    I also bumped the `package.json` version bump in anticipation of publishing a preview release once this change lands. (Hoping to avoid additional PR review delay.)

--- packages/replayio/src/utils/prompt/shouldPrompt.ts
+++ packages/replayio/src/utils/prompt/shouldPrompt.ts
@@ -4,12 +4,33 @@ import { PromptHistory } from "./types";
 
 const ONE_DAY = 1000 * 60 * 60 * 24;
 
-export function shouldPrompt(id: string, minimumIntervalMs: number = ONE_DAY) {
+export function shouldPrompt({
+  id,
+  metadata: metadataNext,
+  minimumIntervalMs = ONE_DAY,
+}: {
+  id: string;
+  metadata: any;
+  minimumIntervalMs?: number;
+}) {
   const cache = readFromCache<PromptHistory>(promptHistoryPath);
   if (!cache) {
     return true;
   }
 
-  const lastPromptTime = cache[id];
-  return lastPromptTime == null || Date.now() - lastPromptTime >= minimumIntervalMs;
+  const entry = cache[id];
+  if (entry == null || typeof entry !== "object") {
+    return true;
+  }
+
+  const { metadata: metadataPrev, time } = entry;
+  if (Date.now() - time >= minimumIntervalMs) {
+    return true;
+  }
+
+  if (metadataNext !== metadataPrev) {
+    return true;
+  }
+
+  return false;
 }
--- packages/replayio/src/utils/promptNpmUpdate.ts
+++ packages/replayio/src/utils/promptNpmUpdate.ts
@@ -2,22 +2,25 @@ import { execSync } from "child_process";
 import { version as currentVersion, name } from "../../package.json";
 import { prompt } from "./prompt/prompt";
 import { shouldPrompt } from "./prompt/shouldPrompt";
+import { updateCachedPromptData } from "./prompt/updateCachedPromptData";
 import { highlight } from "./theme";
 
 const PROMPT_ID = "npm-update";
 
 export async function promptNpmUpdate() {
-  if (!shouldPrompt(PROMPT_ID)) {
-    return;
-  }
-
   try {
     const text = execSync(`npm info ${name} --json`, {
       encoding: "utf8",
       stdio: "pipe",
     }).trim();
     const { version: latestVersion } = JSON.parse(text);
-    if (currentVersion !== latestVersion) {
+    if (
+      currentVersion !== latestVersion &&
+      shouldPrompt({
+        id: PROMPT_ID,
+        metadata: latestVersion,
+      })
+    ) {
       console.log("");
       console.log("A new version of replayio is available!");
       console.log("  Installed version:", highlight(currentVersion));
@@ -29,7 +32,12 @@ export async function promptNpmUpdate() {
       console.log("Press any key to continue");
       console.log("");
 
-      await prompt({ id: PROMPT_ID });
+      await prompt();
+
+      updateCachedPromptData({
+        id: PROMPT_ID,
+        metadata: latestVersion,
+      });
     }
   } catch (error) {
     console.error(error);
