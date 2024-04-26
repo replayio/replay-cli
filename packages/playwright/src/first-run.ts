2ee843e38c962466e55b92fc65bf276ca474e1bb
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-04-26 14:52:56 +0200

    fixed lint

--- packages/playwright/src/first-run.ts
+++ packages/playwright/src/first-run.ts
@@ -1,9 +1,6 @@
 import install from "./install";
 
-if (
-  !process.env.PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD &&
-  !process.env.REPLAY_SKIP_BROWSER_DOWNLOAD
-) {
+if (!process.env.PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD && !process.env.REPLAY_SKIP_BROWSER_DOWNLOAD) {
   console.log("Installing Replay browsers for playwright");
   install("all").then(
     () => {
