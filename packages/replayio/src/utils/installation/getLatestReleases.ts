37582cc77ff46a7333d3dcd970d76bc23e7ac993
Dan Miller <dan@replay.io>
2024-05-13 11:43:21 -0700

    Take release architecture in to account when getting latest release (#451)

--- packages/replayio/src/utils/installation/getLatestReleases.ts
+++ packages/replayio/src/utils/installation/getLatestReleases.ts
@@ -5,26 +5,22 @@ import { runtimeMetadata } from "./config";
 import { debug } from "./debug";
 import { Release } from "./types";
 
-const { platform, runtime } = runtimeMetadata;
+const { architecture, platform, runtime } = runtimeMetadata;
 
 export async function getLatestRelease() {
   debug("Fetching release metadata");
 
   const response = await fetch(`${replayAppHost}/api/releases`);
   const json = (await response.json()) as Release[];
-  const latestRelease = json.find(release => {
-    if (release.platform === platform && release.runtime === runtime) {
-      if (platform === "macOS") {
-        return process.arch.startsWith("arm") === release.releaseFile.includes("arm");
-      } else {
-        return true;
-      }
-    }
-  });
+  const latestRelease = json.find(
+    release =>
+      release.platform === platform &&
+      release.runtime === runtime &&
+      (release.architecture === architecture || release.architecture === "unknown")
+  );
 
+  debug("Latest release", latestRelease);
   assert(latestRelease, `No release found for ${platform}:${runtime}`);
-
-  debug("Latest release on", new Date(latestRelease.time).toLocaleDateString());
 
   return latestRelease;
 }
--- packages/replayio/src/utils/installation/types.ts
+++ packages/replayio/src/utils/installation/types.ts
@@ -4,7 +4,10 @@ export type Platform = "macOS" | "linux" | "windows";
 // This CLI only supports Chromium for the time being
 export type Runtime = "chromium" | "node";
 
+export type Architecture = "arm" | "x86_64" | "unknown";
+
 export type Release = {
+  architecture: Architecture;
   buildFile: string;
   buildId: string;
   platform: Platform;
