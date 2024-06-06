75ee1cb037de7e86f047ffe79dc84b1423c28aaa
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-06-06 09:12:11 -0400

    Show reason for unusable recording (#439)
    When a recording is marked as unusable, the CLI should report that to the user.

--- packages/replayio/src/utils/recordings/readRecordingLog.ts
+++ packages/replayio/src/utils/recordings/readRecordingLog.ts
@@ -1,7 +1,26 @@
 import { readFileSync } from "fs-extra";
 import { recordingLogPath } from "./config";
 import { debug } from "./debug";
-import { LogEntry } from "./types";
+import { LogEntry, RECORDING_LOG_KIND } from "./types";
+
+const RECORDING_LOG_KINDS = [
+  RECORDING_LOG_KIND.createRecording,
+  RECORDING_LOG_KIND.addMetadata,
+  RECORDING_LOG_KIND.writeStarted,
+  RECORDING_LOG_KIND.sourcemapAdded,
+  RECORDING_LOG_KIND.originalSourceAdded,
+  RECORDING_LOG_KIND.writeFinished,
+  RECORDING_LOG_KIND.uploadStarted,
+  RECORDING_LOG_KIND.uploadFinished,
+  RECORDING_LOG_KIND.uploadFailed,
+  RECORDING_LOG_KIND.recordingUnusable,
+  RECORDING_LOG_KIND.crashed,
+  RECORDING_LOG_KIND.crashData,
+  RECORDING_LOG_KIND.crashUploaded,
+  RECORDING_LOG_KIND.processingStarted,
+  RECORDING_LOG_KIND.processingFinished,
+  RECORDING_LOG_KIND.processingFailed,
+];
 
 export function readRecordingLog() {
   const rawText = readFileSync(recordingLogPath, "utf8");
@@ -34,5 +53,6 @@ export function readRecordingLog() {
         });
       }
     })
-    .filter((v): v is LogEntry => !!v);
+    .filter((value): value is LogEntry => !!value)
+    .sort((a, b) => RECORDING_LOG_KINDS.indexOf(a.kind) - RECORDING_LOG_KINDS.indexOf(b.kind));
 }
