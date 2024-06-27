b3d924ffe4977bcda21008d066d0a122f294ac4d
Aditya Pandey <aditya@autospace.co>
2024-06-27 14:20:50 -0500

    Resolve cache path

--- packages/shared/src/logger/identifier/getDeviceId.ts
+++ packages/shared/src/logger/identifier/getDeviceId.ts
@@ -1,4 +1,4 @@
-import { readFromCache, writeToCache } from "../cache";
+import { readFromCache, writeToCache } from "../../cache";
 import { cachePath } from "./cachePath";
 import { randomUUID } from "crypto";
 
