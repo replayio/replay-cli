c6b48ab205dcfef705b3ab007c7dd089ea44b1c1
Miriam Budayr <35577323+miriambudayr@users.noreply.github.com>
2024-07-01 17:08:42 -0500

    add getErrorTags util function

--- packages/test-utils/src/legacy-cli/error.ts
+++ packages/test-utils/src/legacy-cli/error.ts
@@ -1,3 +1,15 @@
 export function getErrorMessage(e: unknown) {
   return e && typeof e === "object" && "message" in e ? (e.message as string) : "Unknown Error";
 }
+
+export function getErrorTags(e: unknown) {
+  if (e instanceof Error) {
+    return {
+      errorMessage: e.message,
+      errorStack: e.stack,
+      errorName: e.name,
+    };
+  }
+
+  return { errorMessage: "Unknown Error" };
+}
