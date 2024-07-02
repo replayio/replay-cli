b439f657fb99cb86577d93316c4e46cb75551683
Aditya Pandey <aditya@autospace.co>
2024-07-02 15:24:31 -0500

    Add assert for eTags and uploadLinks

--- packages/test-utils/src/legacy-cli/main.ts
+++ packages/test-utils/src/legacy-cli/main.ts
@@ -2,6 +2,7 @@ import { retryWithExponentialBackoff } from "@replay-cli/shared/async/retryOnFai
 import fs from "fs";
 import dbg from "./debug";
 import { getHttpAgent } from "./utils";
+import assert from "node:assert/strict";
 
 // requiring v4 explicitly because it's the last version with commonjs support.
 // Should be upgraded to the latest when converting this code to es modules.
@@ -225,6 +226,8 @@ async function multipartUploadRecording(
     chunkSize,
     agentOptions
   );
+
+  assert(eTags.length === partLinks.length, "Mismatched eTags and partLinks");
 
   await client.connectionEndRecordingMultipartUpload(recording.id, uploadId, eTags);
   return recordingId;
