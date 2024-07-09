53c2258ee63d6ea4f182b5c5f3425ac59620884e
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-07-09 14:08:55 +0200

    Properly await all exit tasks (#611)

--- packages/shared/src/process/exitProcess.ts
+++ packages/shared/src/process/exitProcess.ts
@@ -1,7 +1,7 @@
 import { exitTasks } from "./exitTask";
 
 export async function exitProcess(code?: number): Promise<never> {
-  await Promise.all(exitTasks);
+  await Promise.all(exitTasks.map(task => task()));
 
   process.exit(code);
 }
