25fd58892823f70a174903e9dd1ec0b047459437
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-07-09 10:11:56 -0400

    Delete legacy CLI package (#616)
    I think this package should be safe to delete now.
    - We don't plan on publishing further updates to it. 
    - "Shared" code that was in this package has been forked to `test-utils/legacy-cli`.
    - The remaining work (e.g. [PRO-730](https://linear.app/replay/issue/PRO-730)) and updating `create-react-app-typescript` will be done in follow up PRs.

--- packages/replay/src/metadata/index.ts
+++
@@ -1,5 +0,0 @@
-export { sanitizeMetadata as sanitize } from "@replay-cli/shared/recording/metadata/sanitizeMetadata";
-export { addMetadata as add } from "@replay-cli/shared/recording/metadata/addMetadata";
-import * as source from "./source";
-import * as test from "./test";
-export { source, test };
